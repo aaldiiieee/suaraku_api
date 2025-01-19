95901103e21e05a4e89e6a27c956e5c4dbf1785b
aaldiiieee <prmdy02@gmail.com>
2025-01-20 05:40:00 +0700

    fix

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(cors(corsOptions));
 
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+export default app;
