a1beb01c21d10b34abb9127cfc3ff16edd452ecd
aaldiiieee <prmdy02@gmail.com>
2025-01-29 14:21:05 +0700

    rollback

--- config/multer.js
+++ config/multer.js
@@ -1,11 +1,5 @@
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
-
-const uploadDir = 'uploads';
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
